d65e7a0b7a700aeb76305979e994bb22779a0455
olcay <olcayeryigit@hotmail.com>
2024-11-28 01:07:04 +0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
-"use client";
+"use client"; // Bu sayfanın istemci tarafında çalışması gerektiğini belirtir.
 
+import dynamic from 'next/dynamic';
 import HomePage from "@/components/home-page/HomePage";
 
 export default function Home() {
   return (
     <div className="">
-  <HomePage/>
+      <HomePage />
     </div>
   );
 }
